b8e1df26c1f02536b44d65ab8a6def7b155883eb
Linnea Olsson <linnea.x.olsson@ri.se>
2024-03-14 15:11:28 +0100

    add icons

--- src/base-elements/navbar/RightDrawer.jsx
+++ src/base-elements/navbar/RightDrawer.jsx
@@ -6,6 +6,9 @@ import { useRecoilState } from "recoil"
 import { Box } from "@mui/material"
 import { ObcAppMenu as AppMenu } from "@oicl/openbridge-webcomponents-react/components/app-menu/app-menu"
 import { ObcAppButton as AppButton } from "@oicl/openbridge-webcomponents-react/components/app-button/app-button"
+import { Obi19Current as CurrentIcon } from "@oicl/openbridge-webcomponents-react/icons/icon-19-current"
+import { Obi15Play as PlayIcon } from "@oicl/openbridge-webcomponents-react/icons/icon-15-play"
+import { Obi03Filter as FilterIcon } from "@oicl/openbridge-webcomponents-react/icons/icon-03-filter"
 
 export default function RightDrawer(props) {
   let [showMiniApp, setShowMiniApp] = useRecoilState(showMiniAppsObj)
@@ -25,14 +28,16 @@ export default function RightDrawer(props) {
       onKeyDown={props.toggleDrawer(props.side, false)}
     >
       <AppMenu>
-        <AppButton
-          checked={showMiniApp.windCurrent}
-          label="Wind & Current"
-          onClick={() => ToggleMiniApp("windCurrent")}
-        ></AppButton>
+        <AppButton checked={showMiniApp.windCurrent} label="Wind & Current" onClick={() => ToggleMiniApp("windCurrent")}>
+          <CurrentIcon size="24" slot="icon" />
+        </AppButton>
 
-        <AppButton checked={showMiniApp.playback} label="Playback" onClick={() => ToggleMiniApp("playback")}></AppButton>
-        <AppButton checked={showMiniApp.controls} label="Controls" onClick={() => ToggleMiniApp("controls")}></AppButton>
+        <AppButton checked={showMiniApp.playback} label="Playback" onClick={() => ToggleMiniApp("playback")}>
+          <PlayIcon size="24" slot="icon" />
+        </AppButton>
+        <AppButton checked={showMiniApp.controls} label="Controls" onClick={() => ToggleMiniApp("controls")}>
+          <FilterIcon size="24" slot="icon" />
+        </AppButton>
       </AppMenu>
     </Box>
   )
