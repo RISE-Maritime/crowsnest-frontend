dbd50805a2f5c7502850cb0e15e6ae92c9eed46b
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-05-15 12:06:42 +0200

    Update ThrustPanel.jsx

--- src/apps/wind-power/components/ThrustPanel.jsx
+++ src/apps/wind-power/components/ThrustPanel.jsx
@@ -3,13 +3,26 @@ import Paper from "@mui/material/Paper"
 import CardHeading from "./CardHeading"
 import { Stack } from "@mui/material"
 import { ObcInstrumentField } from "@oicl/openbridge-webcomponents-react/navigation-instruments/instrument-field/instrument-field"
+import ControlThrust from "./ControlThrust"
 
 export default function ThrustPanel() {
   return (
     <Paper>
-      <Stack>
-        <CardHeading heading="Thrust Panel" />
-        <ObcInstrumentField />
+      <Stack direction="column" justifyContent="center" alignItems="center" spacing={0}>
+        <CardHeading heading="Thrust" />
+        <ObcInstrumentField
+          value={5}
+          hasSetpoint={true}
+          setpoint={10}
+          degree={false}
+          tag={"POWER"}
+          unit={"%"}
+          source={"simulation"}
+          hasSource={false}
+          size={"large"}
+          maxDigits={3}
+        />
+        <ControlThrust />
       </Stack>
     </Paper>
   )
