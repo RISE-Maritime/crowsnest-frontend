d3f8a2de0b88f531fd4b6b9b323dab976c0e5abb
Linnea Olsson <linnea.x.olsson@ri.se>
2024-05-23 13:29:14 +0200

    minor visual adjustments

--- src/apps/wind-power/index.jsx
+++ src/apps/wind-power/index.jsx
@@ -55,13 +55,13 @@ export default function index() {
 
   return (
     <Grid container sx={{ padding: "0.25rem" }} spacing={0.5}>
-      <Grid item xs={3} sx={{ height: "calc(100vh - 58px)" }}>
+      <Grid item xs={3} sx={{ minHeight: "calc(100vh - 58px)" }}>
         <ThrustPanel />
       </Grid>
-      <Grid item xs sx={{ height: "calc(100vh - 58px)" }}>
+      <Grid item xs sx={{ minHeight: "calc(100vh - 58px)" }}>
         <SailPositions />
       </Grid>
-      <Grid item xs={6} sx={{ height: "calc(100vh - 58px)" }}>
+      <Grid item xs={6} sx={{ minHeight: "calc(100vh - 58px)" }}>
         <PanelSail />
       </Grid>
     </Grid>
