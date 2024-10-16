db3e7371e0005b0b8f31172a68d22fc6ee28729d
Linnea Olsson <linnea.x.olsson@ri.se>
2024-10-16 10:36:47 +0200

    Redo and use ObcInstrumentField for displaying ship values

--- src/apps/ECDIS/components/OsInfo.jsx
+++ src/apps/ECDIS/components/OsInfo.jsx
@@ -1,62 +1,89 @@
 import React from "react"
+import { styled } from "@mui/material/styles"
 import { Stack, FormControl, InputLabel, Select, MenuItem, Typography, Grid } from "@mui/material"
 import { useRecoilState, useRecoilValue } from "recoil"
 import { OS_POSITION_SETTING, OS_VELOCITY_SETTING, OS_VELOCITY, OS_HEADING, OS_HEADING_SETTING } from "../../../recoil/atoms"
 
+import Paper from "@mui/material/Paper"
+import ContainerHeading from "../../../base-elements/components/ContainerHeading"
+import PositionStatusSmall from "./PositionStatusSmall"
+
+import { ObcInstrumentField } from "@oicl/openbridge-webcomponents-react/navigation-instruments/instrument-field/instrument-field"
+
 function handleDataField(data, field) {
   if (data !== undefined && data !== null) {
-    return data[field] !== null && data[field] !== undefined ? data[field].toString() : "---"
+    return data[field] !== null && data[field] !== undefined ? data[field].toString() : 0
   } else {
-    return "---"
+    return 0
   }
 }
 
+const Tag = styled(Typography)`
+  font-family: "Noto Sans";
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.5rem;
+  color: var(--instrument-regular-secondary-color);
+`
+
 export default function OsInfo({ data, identifier }) {
   return (
-    <div>
-      <hr style={{ width: "100%" }} />
+    <Paper sx={{ height: "100%" }}>
+      <ContainerHeading heading="Own ship" />
       <Grid container sx={{ padding: "0.5rem" }}>
         <Grid item xs={12}>
-          <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-            <Typography variant="subtitle1">OS info</Typography>
+          <Stack direction="row" justifyContent="space-between">
+            icon
+            <Stack direction="row" spacing={1}>
+              <ObcInstrumentField hasSetpoint={false} degree value={handleDataField(data[identifier], "heading")} tag="" />
+              <Tag>HDG</Tag>
+            </Stack>
+            select
           </Stack>
         </Grid>
 
         <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={6}>
-              <div style={{ textAlign: "center" }}>
-                <Typography variant="caption">Heading</Typography>
-                <Typography variant="subtitle1">{handleDataField(data[identifier], "heading")}°</Typography>
-              </div>
-            </Grid>
+          <Stack direction="row" justifyContent="space-between">
+            icon
+            <Stack direction="row" spacing={1}>
+              <ObcInstrumentField hasSetpoint={false} degree value={handleDataField(data[identifier], "course")} tag="" />
+              <Tag>COG</Tag>
+            </Stack>
+            select
+          </Stack>
+        </Grid>
 
-            <Grid item xs={6}>
-              <div style={{ textAlign: "center" }}>
-                <Typography variant="caption">COG</Typography>
-                <Typography variant="subtitle1">{handleDataField(data[identifier], "course")}°</Typography>
-              </div>
-            </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" justifyContent="space-between">
+            icon
+            <Stack direction="row" spacing={1}>
+              <ObcInstrumentField hasSetpoint={false} degree value={handleDataField(data[identifier], "speed")} tag="" />
+              <Tag>SOG</Tag>
+            </Stack>
+            select
+          </Stack>
+        </Grid>
 
-            <Grid item xs={6}>
-              <div style={{ textAlign: "center" }}>
-                <Typography variant="caption">SOG</Typography>
-                <Typography variant="subtitle1">{handleDataField(data[identifier], "speed")}kts</Typography>
-              </div>
-            </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" justifyContent="space-between">
+            icon
+            <Stack direction="row" spacing={1}>
+              <ObcInstrumentField hasSetpoint={false} degree value={handleDataField(data[identifier], "rate")} tag="" />
+              <Tag>
+                ROT
+                <br />
+                /min
+              </Tag>
+            </Stack>
+            select
+          </Stack>
+        </Grid>
 
-            <Grid item xs={6}>
-              <div style={{ textAlign: "center" }}>
-                <Typography variant="caption">ROT</Typography>
-                <Typography variant="subtitle1">{handleDataField(data[identifier], "rate")}°/min</Typography>
-              </div>
-            </Grid>
-          </Grid>
-
-          <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}></Stack>
+        <Grid item xs={12}>
+          <PositionStatusSmall />
         </Grid>
       </Grid>
-      <hr style={{ width: "100%" }} />
-    </div>
+    </Paper>
   )
 }
